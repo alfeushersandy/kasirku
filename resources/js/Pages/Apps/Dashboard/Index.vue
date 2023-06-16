7458ba50319a9008ca092a8fa694ea080a2d677a
unknown <alfeusputra@gmail.com>
2023-06-16 10:25:17 +0700

    menambah daftar permission

--- resources/js/Pages/Apps/Dashboard/Index.vue
+++ resources/js/Pages/Apps/Dashboard/Index.vue
@@ -1,7 +1,7 @@
 <template>
 
     <Head>
-        <title>Dashboard - Aplikasi Kasir</title>
+        <title>Dashboard - KASIRKU</title>
     </Head>
 
     <main class="c-main">
@@ -15,6 +15,7 @@
                                 <span class="font-weight-bold"><i class="fa fa-chart-bar"></i> SALES CHART 7 DAYS</span>
                             </div>
                             <div class="card-body">
+                                <BarChart :chartData="chartSellWeek" :options="options" />
                             </div>
                         </div>
                     </div>
@@ -50,7 +51,7 @@
                                 <span class="font-weight-bold"><i class="fa fa-chart-pie"></i> BEST SELIING PRODUCT</span>
                             </div>
                             <div class="card-body">
-
+                                <DoughnutChart :chartData="chartBestProduct" />
                             </div>
                         </div>
                     </div>
@@ -60,6 +61,20 @@
                                 <span class="font-weight-bold"><i class="fa fa-box-open"></i> PRODUCT STOCK</span>
                             </div>
                             <div class="card-body">
+                                <div v-if="products_limit_stock.length > 0">
+                                    <ol class="list-group list-group-numbered">
+                                        <li v-for="product in products_limit_stock" :key="product.id" class="list-group-item d-flex justify-content-between align-items-start">
+                                            <div class="ms-2 me-auto">
+                                                <div class="fw-bold">{{ product.title }}</div>
+                                                <div class="text-muted">Category : {{ product.category.name }}</div>
+                                            </div>
+                                            <span class="badge bg-danger rounded-pill">{{ product.stock }}</span>
+                                        </li>
+                                    </ol>
+                                </div>
+                                <div v-else class="alert alert-danger border-0 shadow rounded-3">
+                                    Data Tidak Tersedia!.
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -77,6 +92,16 @@
     //import Heade and useForm from Inertia
     import { Head } from '@inertiajs/inertia-vue3';
 
+    //import ref from vue
+    import { ref } from 'vue';
+
+    //chart
+    import { BarChart, DoughnutChart } from 'vue-chart-3';
+    import { Chart, registerables } from "chart.js";
+
+    //register chart
+    Chart.register(...registerables);
+
     export default {
 
         //layout
@@ -85,6 +110,8 @@
         //register component
         components: {
             Head,
+            BarChart,
+            DoughnutChart
         },
 
         props: {
@@ -96,6 +123,78 @@
 
             //jumlah profit/laba hari ini
             sum_profits_today: Number,
+
+            //chart sales
+            sales_date: Array,
+            grand_total: Array,
+
+            //produk terlaris
+            product: Array,
+            total: Array,
+
+            //produk limit stock
+            products_limit_stock: Array,
+        },
+
+        setup(props) {
+
+            //method random color
+            function randomBackgroundColor(length) {
+                var data = [];
+                for (var i = 0; i < length; i++) {
+                    data.push(getRandomColor());
+                }
+                return data;
+            }
+
+            //method generate random color
+            function getRandomColor() {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
+
+            //option chart
+            const options = ref({
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                    title: {
+                        display: false,
+                    },
+                },
+                beginZero: true
+            });
+
+            //chart sell week
+            const chartSellWeek = {
+                labels: props.sales_date,
+                datasets: [{
+                    data: props.grand_total,
+                    backgroundColor: randomBackgroundColor(props.sales_date.length),
+                }, ],
+            };
+
+            //chart produk terlaris
+            const chartBestProduct = {
+                labels: props.product,
+                datasets: [{
+                    data: props.total,
+                    backgroundColor: randomBackgroundColor(5),
+                }, ],
+            };
+
+            return {
+                options,
+                chartSellWeek,
+                chartBestProduct
+            }
+
         }
     }
 </script>
